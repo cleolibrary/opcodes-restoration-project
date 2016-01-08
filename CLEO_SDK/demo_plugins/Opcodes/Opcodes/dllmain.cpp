5296f5fb7b2468529dbe5ff3d55ae69dd4810073
spaceeinstein <spaceeinstein@yahoo.com>
2016-01-08 15:10:21 -0500

    Add opcodes for Vice City
    - 0178 IS_PLAYER_TOUCHING_OBJECT
    - 0179 IS_CHAR_TOUCHING_OBJECT
    - 023A IS_PLAYER_TOUCHING_OBJECT_ON_FOOT
    - 023B IS_CHAR_TOUCHING_OBJECT_ON_FOOT
    - 029C IS_BOAT
    - 02A0 IS_CHAR_STOPPED
    - 04A9 IS_CHAR_IN_ANY_HELI
    - 04AB IS_CHAR_IN_ANY_PLANE
    - 04C8 IS_CHAR_IN_FLYING_VEHICLE
    - 0547 IS_CHAR_TOUCHING_VEHICLE

--- CLEO_SDK/demo_plugins/Opcodes/Opcodes/dllmain.cpp
+++ CLEO_SDK/demo_plugins/Opcodes/Opcodes/dllmain.cpp
@@ -86,6 +86,28 @@ uintptr_t getVehicleStruct(unsigned int param)
 	return pfVehiclePoolGetStruct(carPool, param);
 }
 
+uintptr_t getObjectStruct(unsigned int param)
+{
+	void *func = NULL;
+	void *objectPool = NULL;
+	switch (getGameVersion()) {
+	case V1_0:
+		func = (void *)0x451C30;
+		objectPool = *(void **)0x94DBE0;
+		break;
+	case V1_1:
+		func = (void *)0x451C30;
+		objectPool = *(void **)0x94DBE8;
+		break;
+	case VSTEAM:
+		func = (void *)0x451B10;
+		objectPool = *(void **)0x94CBE8;
+		break;
+	}
+	auto pfObjectPoolGetStruct = (uintptr_t(__thiscall *)(void *, DWORD))func; // cpool_cobject_ccutscenehead::getat
+	return pfObjectPoolGetStruct(objectPool, param);
+}
+
 DWORD getPlayerPed(unsigned int param)
 {
 	uintptr_t *address = NULL;
@@ -286,6 +308,70 @@ eOpcodeResult WINAPI IS_CAR_IN_AIR(CScript *script)
 	return OR_CONTINUE;
 }
 
+/* 0178 */
+eOpcodeResult WINAPI IS_PLAYER_TOUCHING_OBJECT(CScript *script)
+{
+	script->Collect(2);
+	void *func = NULL;
+	switch (getGameVersion()) {
+	case V1_0:
+		func = (void *)0x4B9010;
+		break;
+	case V1_1:
+		func = (void *)0x4B9030;
+		break;
+	default:
+		return OR_CONTINUE; // todo steam address
+	}
+	DWORD source = getPlayerPed(Params[0].nVar);
+	uintptr_t target = getObjectStruct(Params[1].nVar);
+	if (*(BYTE *)(source + 0x3AC)) {
+		uintptr_t car = *(uintptr_t *)(source + 0x3A8);
+		if (car != NULL) {
+			source = car;
+		}
+	}
+	auto GetHasCollidedWith = (BYTE(__thiscall *)(DWORD, uintptr_t))func; // cphysical::gethascollidedwith
+	if (GetHasCollidedWith(source, target)) {
+		script->UpdateCompareFlag(true);
+		return OR_CONTINUE;
+	}
+	script->UpdateCompareFlag(false);
+	return OR_CONTINUE;
+}
+
+/* 0179 */
+eOpcodeResult WINAPI IS_CHAR_TOUCHING_OBJECT(CScript *script)
+{
+	script->Collect(2);
+	void *func = NULL;
+	switch (getGameVersion()) {
+	case V1_0:
+		func = (void *)0x4B9010;
+		break;
+	case V1_1:
+		func = (void *)0x4B9030;
+		break;
+	default:
+		return OR_CONTINUE; // todo steam address
+	}
+	uintptr_t source = getPedStruct(Params[0].nVar);
+	uintptr_t target = getObjectStruct(Params[1].nVar);
+	if (*(BYTE *)(source + 0x3AC)) {
+		uintptr_t car = *(uintptr_t *)(source + 0x3A8);
+		if (car != NULL) {
+			source = car;
+		}
+	}
+	auto GetHasCollidedWith = (BYTE(__thiscall *)(uintptr_t, uintptr_t))func; // cphysical::gethascollidedwith
+	if (GetHasCollidedWith(source, target)) {
+		script->UpdateCompareFlag(true);
+		return OR_CONTINUE;
+	}
+	script->UpdateCompareFlag(false);
+	return OR_CONTINUE;
+}
+
 /* 017B */
 eOpcodeResult WINAPI SET_CHAR_AMMO(CScript *script)
 {
@@ -378,6 +464,62 @@ eOpcodeResult WINAPI IS_CAR_ARMED_WITH_BOMB(CScript *script)
 	return OR_CONTINUE;
 }
 
+/* 023A */
+eOpcodeResult WINAPI IS_PLAYER_TOUCHING_OBJECT_ON_FOOT(CScript *script)
+{
+	script->Collect(2);
+	void *func = NULL;
+	switch (getGameVersion()) {
+	case V1_0:
+		func = (void *)0x4B9010;
+		break;
+	case V1_1:
+		func = (void *)0x4B9030;
+		break;
+	default:
+		return OR_CONTINUE; // todo steam address
+	}
+	DWORD player = getPlayerPed(Params[0].nVar);
+	uintptr_t object = getObjectStruct(Params[1].nVar);
+	if (!*(BYTE *)(player + 0x3AC)) {
+		auto GetHasCollidedWith = (BYTE(__thiscall *)(DWORD, uintptr_t))func; // cphysical::gethascollidedwith
+		if (GetHasCollidedWith(player, object)) {
+			script->UpdateCompareFlag(true);
+			return OR_CONTINUE;
+		}
+	}
+	script->UpdateCompareFlag(false);
+	return OR_CONTINUE;
+}
+
+/* 023B */
+eOpcodeResult WINAPI IS_CHAR_TOUCHING_OBJECT_ON_FOOT(CScript *script)
+{
+	script->Collect(2);
+	void *func = NULL;
+	switch (getGameVersion()) {
+	case V1_0:
+		func = (void *)0x4B9010;
+		break;
+	case V1_1:
+		func = (void *)0x4B9030;
+		break;
+	default:
+		return OR_CONTINUE; // todo steam address
+	}
+	uintptr_t ped = getPedStruct(Params[0].nVar);
+	uintptr_t object = getObjectStruct(Params[1].nVar);
+	if (!*(BYTE *)(ped + 0x3AC)) {
+		auto GetHasCollidedWith = (BYTE(__thiscall *)(uintptr_t, uintptr_t))func; // cphysical::gethascollidedwith
+		if (GetHasCollidedWith(ped, object)) {
+			script->UpdateCompareFlag(true);
+			return OR_CONTINUE;
+		}
+	}
+	script->UpdateCompareFlag(false);
+	return OR_CONTINUE;
+}
+
 /* 0242 */
 eOpcodeResult WINAPI ARM_CAR_WITH_BOMB(CScript *script)
 {
@@ -387,6 +529,40 @@ eOpcodeResult WINAPI ARM_CAR_WITH_BOMB(CScript *script)
 	BYTE state = (*(BYTE *)(car + 0x1FE) & 0xF8);
 	state |= input;
 	*(BYTE *)(car + 0x1FE) = state;
+	return OR_CONTINUE;
+}
+
+/* 029C */
+eOpcodeResult WINAPI IS_BOAT(CScript *script)
+{
+	script->Collect(1);
+	uintptr_t car = getVehicleStruct(Params[0].nVar);
+	if (*(DWORD *)(car + 0x29C) == 1) {
+		script->UpdateCompareFlag(true);
+		return OR_CONTINUE;
+	}
+	script->UpdateCompareFlag(false);
+	return OR_CONTINUE;
+}
+
+/* 02A0 */
+eOpcodeResult WINAPI IS_CHAR_STOPPED(CScript *script)
+{
+	script->Collect(1);
+	uintptr_t ped = getPedStruct(Params[0].nVar);
+	if (*(BYTE *)(ped + 0x3AC)) {
+		uintptr_t car = *(uintptr_t *)(ped + 0x3A8);
+		if (*(float *)(car + 0x100) == 0.0) {
+			script->UpdateCompareFlag(true);
+			return OR_CONTINUE;
+		}
+	} else {
+		if (*(DWORD *)(ped + 0x24C) <= 1) {
+			script->UpdateCompareFlag(true);
+			return OR_CONTINUE;
+		}
+	}
+	script->UpdateCompareFlag(false);
 	return OR_CONTINUE;
 }
 
@@ -497,6 +673,75 @@ eOpcodeResult WINAPI IS_CHAR_IN_ANY_BOAT(CScript *script)
 	return OR_CONTINUE;
 }
 
+/* 04A9 */
+eOpcodeResult WINAPI IS_CHAR_IN_ANY_HELI(CScript *script)
+{
+	script->Collect(1);
+	uintptr_t ped = getPedStruct(Params[0].nVar);
+	if (ped != NULL) {
+		if (*(BYTE *)(ped + 0x3AC)) {
+			uintptr_t car = *(uintptr_t *)(ped + 0x3A8);
+			if (car != NULL) {
+				DWORD handling = *(DWORD *)(car + 0x120);
+				DWORD flags = *(DWORD *)(handling + 0xCC);
+				flags &= 0xF0000;
+				if (flags == 0x20000) {
+					script->UpdateCompareFlag(true);
+					return OR_CONTINUE;
+				}
+			}
+		}
+	}
+	script->UpdateCompareFlag(false);
+	return OR_CONTINUE;
+}
+
+/* 04AB */
+eOpcodeResult WINAPI IS_CHAR_IN_ANY_PLANE(CScript *script)
+{
+	script->Collect(1);
+	uintptr_t ped = getPedStruct(Params[0].nVar);
+	if (ped != NULL) {
+		if (*(BYTE *)(ped + 0x3AC)) {
+			uintptr_t car = *(uintptr_t *)(ped + 0x3A8);
+			if (car != NULL) {
+				DWORD handling = *(DWORD *)(car + 0x120);
+				DWORD flags = *(DWORD *)(handling + 0xCC);
+				flags &= 0xF0000;
+				if (flags == 0x40000) {
+					script->UpdateCompareFlag(true);
+					return OR_CONTINUE;
+				}
+			}
+		}
+	}
+	script->UpdateCompareFlag(false);
+	return OR_CONTINUE;
+}
+
+/* 04C8 */
+eOpcodeResult WINAPI IS_CHAR_IN_FLYING_VEHICLE(CScript *script)
+{
+	script->Collect(1);
+	uintptr_t ped = getPedStruct(Params[0].nVar);
+	if (ped != NULL) {
+		if (*(BYTE *)(ped + 0x3AC)) {
+			uintptr_t car = *(uintptr_t *)(ped + 0x3A8);
+			if (car != NULL) {
+				DWORD handling = *(DWORD *)(car + 0x120);
+				DWORD flags = *(DWORD *)(handling + 0xCC);
+				flags &= 0xF0000;
+				if (flags == 0x20000 || flags == 0x40000) {
+					script->UpdateCompareFlag(true);
+					return OR_CONTINUE;
+				}
+			}
+		}
+	}
+	script->UpdateCompareFlag(false);
+	return OR_CONTINUE;
+}
+
 /* 050F */
 eOpcodeResult WINAPI GET_MAX_WANTED_LEVEL(CScript *script)
 {
@@ -513,6 +758,38 @@ eOpcodeResult WINAPI GET_MAX_WANTED_LEVEL(CScript *script)
 	}
 	Params[0].nVar = *(DWORD *)address;
 	script->Store(1);
+	return OR_CONTINUE;
+}
+
+/* 0547 */
+eOpcodeResult WINAPI IS_CHAR_TOUCHING_VEHICLE(CScript *script)
+{
+	script->Collect(2);
+	void *func = NULL;
+	switch (getGameVersion()) {
+	case V1_0:
+		func = (void *)0x4B9010;
+		break;
+	case V1_1:
+		func = (void *)0x4B9030;
+		break;
+	default:
+		return OR_CONTINUE; // todo steam address
+	}
+	uintptr_t source = getPedStruct(Params[0].nVar);
+	uintptr_t target = getVehicleStruct(Params[1].nVar);
+	if (*(BYTE *)(source + 0x3AC)) {
+		uintptr_t car = *(uintptr_t *)(source + 0x3A8);
+		if (car != NULL) {
+			source = car;
+		}
+	}
+	auto GetHasCollidedWith = (BYTE(__thiscall *)(uintptr_t, uintptr_t))func; // cphysical::gethascollidedwith
+	if (GetHasCollidedWith(source, target)) {
+		script->UpdateCompareFlag(true);
+		return OR_CONTINUE;
+	}
+	script->UpdateCompareFlag(false);
 	return OR_CONTINUE;
 }
 
@@ -538,18 +815,28 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 		Opcodes::RegisterOpcode(0x0135, CHANGE_CAR_LOCK);
 		Opcodes::RegisterOpcode(0x0136, SHAKE_CAM_WITH_POINT);
 		Opcodes::RegisterOpcode(0x015E, IS_CAR_IN_AIR);
+		Opcodes::RegisterOpcode(0x0178, IS_PLAYER_TOUCHING_OBJECT);
+		Opcodes::RegisterOpcode(0x0179, IS_CHAR_TOUCHING_OBJECT);
 		Opcodes::RegisterOpcode(0x017B, SET_CHAR_AMMO);
 		Opcodes::RegisterOpcode(0x021D, SET_FREE_BOMBS);
 		Opcodes::RegisterOpcode(0x021F, SET_ALL_TAXI_LIGHTS);
 		Opcodes::RegisterOpcode(0x0220, IS_CAR_ARMED_WITH_ANY_BOMB);
 		Opcodes::RegisterOpcode(0x0228, IS_CAR_ARMED_WITH_BOMB);
+		Opcodes::RegisterOpcode(0x023A, IS_PLAYER_TOUCHING_OBJECT_ON_FOOT);
+		Opcodes::RegisterOpcode(0x023B, IS_CHAR_TOUCHING_OBJECT_ON_FOOT);
 		Opcodes::RegisterOpcode(0x0242, ARM_CAR_WITH_BOMB);
+		Opcodes::RegisterOpcode(0x029C, IS_BOAT);
+		Opcodes::RegisterOpcode(0x02A0, IS_CHAR_STOPPED);
 		Opcodes::RegisterOpcode(0x02F0, DROP_MINE);
 		Opcodes::RegisterOpcode(0x02F1, DROP_NAUTICAL_MINE);
 		Opcodes::RegisterOpcode(0x031B, IS_FIRST_CAR_COLOUR);
 		Opcodes::RegisterOpcode(0x031C, IS_SECOND_CAR_COLOUR);
 		Opcodes::RegisterOpcode(0x04A7, IS_CHAR_IN_ANY_BOAT);
+		Opcodes::RegisterOpcode(0x04A9, IS_CHAR_IN_ANY_HELI);
+		Opcodes::RegisterOpcode(0x04AB, IS_CHAR_IN_ANY_PLANE);
+		Opcodes::RegisterOpcode(0x04C8, IS_CHAR_IN_FLYING_VEHICLE);
 		Opcodes::RegisterOpcode(0x050F, GET_MAX_WANTED_LEVEL);
+		Opcodes::RegisterOpcode(0x0547, IS_CHAR_TOUCHING_VEHICLE);
 	}
 	return TRUE;
 }
